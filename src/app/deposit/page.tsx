019ed3f3b895ca4c43ff9dc30f4468a7443ae6fa
Seun Oyediran <seunoyediran22517@gmail.com>
2026-03-25 09:52:49 +0100

    fix: sizing fix

--- src/app/deposit/page.tsx
+++ src/app/deposit/page.tsx
@@ -7,22 +7,24 @@ export default function DepositPage() {
   const [showDepositModal, setShowDepositModal] = useState(false);
 
   return (
-    <div className="relative flex items-center justify-center bg-[#0C0C0C] h-screen overflow-hidden">
-      <div className="w-full max-w-[320px] flex flex-col gap-4">
-        <Swap handleClick={() => setShowDepositModal(true)} />
-        <button
-          type="button"
-          onClick={() => setShowDepositModal(true)}
-          className="w-full px-5 py-2.5 rounded-xl bg-[rgba(249,249,249,0.08)] text-[#F9F9F9] text-sm font-medium tracking-[-0.004em] cursor-pointer"
-        >
-          Deposit
-        </button>
-      </div>
+    <div className="flex items-center justify-center bg-[#0C0C0C] h-screen overflow-hidden">
+      <div className="w-full max-w-[320px] h-135 relative overflow-hidden flex flex-col justify-center">
+        <div className="w-full max-w-75 mx-auto flex flex-col gap-4 ">
+          <Swap handleClick={() => setShowDepositModal(true)} />
+          <button
+            type="button"
+            onClick={() => setShowDepositModal(true)}
+            className="w-full px-5 py-2.5 rounded-xl bg-[rgba(249,249,249,0.08)] text-[#F9F9F9] text-sm font-medium tracking-[-0.004em] cursor-pointer"
+          >
+            Deposit
+          </button>
+        </div>
 
-      <DepositFlow
-        showDepositModal={showDepositModal}
-        setShowDepositModal={setShowDepositModal}
-      />
+        <DepositFlow
+          showDepositModal={showDepositModal}
+          setShowDepositModal={setShowDepositModal}
+        />
+      </div>
     </div>
   );
 }
