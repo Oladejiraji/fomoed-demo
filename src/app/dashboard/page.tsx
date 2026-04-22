65a53a7913b92644a78feb6375df971f7cb97ab1
Seun Oyediran <seunoyediran22517@gmail.com>
2026-04-22 12:34:50 +0100

    feat: dashboard grid layout added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,10 @@
+import { DashboardGridLayout } from "@/components/dashboard/dashboard-grid-layout";
 import React from "react";
 
 export default function Page() {
   return (
-    <div>
-      <p>hello from dashboard</p>
+    <div className="flex-1 bg flex overflow-hidden">
+      <DashboardGridLayout />
     </div>
   );
 }
